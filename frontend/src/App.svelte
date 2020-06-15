7493774a5dfab6517dab4be75fd9b10bc496f3ee
vbstreetz <vbstreetz@gmail.com>
2020-06-15 04:53:51 +0300

    chore: try fix social buttons

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -7,17 +7,30 @@
   .topbar {
     padding: 20px 0;
   }
-  a {
+  .link {
     color: #888;
   }
-  a:visited,
-  a:hover {
+  .link:visited,
+  .link:hover {
     opacity: 0.6;
   }
 </style>
 
 <div class="topbar flex justify-end text-xs">
-  <a href="/" class="mr-3">bet</a>
-  <a href="/prices" class="mr-3">prices</a>
+  <a class="mr-3 link" href="/">bet</a>
+  <a class="mr-3 link" href="/prices">prices</a>
+
+  <div class="mr-3">
+    <a class="github-button"
+      href="https://github.com/vbstreetz/coin-price-bet"
+      aria-label="Star vbstreetz/coin-price-bet on GitHub"
+      target="_blank">Star</a>
+  </div>
+
+  <a
+    class="twitter-follow-button"
+    href="https://twitter.com/coin_price_bet?ref_src=twsrc%5Etfw"
+    data-show-count="false"
+    target="_blank">Follow @coin_price_bet</a>
 </div>
 <Router {routes}/>
