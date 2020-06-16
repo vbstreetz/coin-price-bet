cc252f9160c79aa66a3ff9b46b98d4b70ba70c7f
vbstreetz <vbstreetz@gmail.com>
2020-06-16 13:24:35 +0300

    disable linking to gaia

--- playground/recreate-relayer.sh
+++ playground/recreate-relayer.sh
@@ -3,15 +3,15 @@ set -e
 rly lite delete band-consumer
 rly lite init band-consumer -f
 
-rly pth delete transfer
+#rly pth delete transfer
 rly pth delete oracle
 
-rly pth gen band-consumer transfer band-cosmoshub transfer transfer
+#rly pth gen band-consumer transfer band-cosmoshub transfer transfer
 rly pth gen band-consumer coinpricebet ibc-bandchain oracle oracle
 
 echo "linking paths..."
 echo "" > ./playground/relayer-create.log
-rly tx link transfer >> ./playground/relayer-create.log
+#rly tx link transfer >> ./playground/relayer-create.log
 rly tx link oracle >> ./playground/relayer-create.log
 
 ./playground/update-env.sh
--- playground/update-env.sh
+++ playground/update-env.sh
@@ -1,19 +1,19 @@
 node ./playground/update-relayer-env.js
 
 export $(cat ./playground/relayers.env | xargs)
-echo $betchain_transfer_channel
-echo $gaia_transfer_channel
+# echo $betchain_transfer_channel
+# echo $gaia_transfer_channel
 echo $betchain_oracle_channel
 echo $bandchain_oracle_channel
 
 
-bccli tx coinpricebet \
-set-channel band-cosmoshub transfer $betchain_transfer_channel \
---from validator --keyring-backend test -y -b block
+# bccli tx coinpricebet \
+# set-channel band-cosmoshub transfer $betchain_transfer_channel \
+# --from validator --keyring-backend test -y -b block
 
-bccli tx coinpricebet \
-set-channel band-consumer transfer $gaia_transfer_channel \
---from validator --keyring-backend test -y -b block
+# bccli tx coinpricebet \
+# set-channel band-consumer transfer $gaia_transfer_channel \
+# --from validator --keyring-backend test -y -b block
 
 bccli tx coinpricebet \
 set-channel ibc-bandchain coinpricebet $betchain_oracle_channel \
